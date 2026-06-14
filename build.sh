9eb22bb89a671aaf76cafb3b2bb690f5d14dc9c6
wenzongze <59761310+wenzongze@users.noreply.github.com>
2026-06-15 03:10:24 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-rm -f go.mod go.sum
+set -ex
+rm -rf go.mod go.sum themes/hugo-theme-stack
 mkdir -p themes
 curl -sL https://github.com/CaiJimmy/hugo-theme-stack/archive/refs/heads/master.tar.gz | tar xz -C themes/
 mv themes/hugo-theme-stack-* themes/hugo-theme-stack
